737f5d2d196b8997c88888285d0e968dc97477fd
Michael Stevens <codemasterflash@Michaels-MacBook-Pro.local>
2020-01-19 17:12:54 -0600

    added acctrouter, serer.js

--- server.js
+++ server.js
@@ -6,4 +6,19 @@ const server = express();
 
 server.use(express.json());
 
+server.use("/accounts", db);
+
+server.get("/", (req, res) => {
+    res.send("<h1>Db one working</h1>")
+})
+server.use((err, req, res, next) => {
+    console.log(err)
+    res.status(500).json({
+        message: "There was an error"
+    })
+})
+
+
+
+
 module.exports = server;
